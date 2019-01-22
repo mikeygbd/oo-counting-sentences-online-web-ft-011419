a7f7a30781d2578cef6eac689a8ca076233756c4
Michael Soares <mikey.soares@gmail.com>
2019-01-22 15:44:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -5,18 +5,29 @@ class String
   def sentence?
     if self.end_with?(".")
       return true
-
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    # binding.pry
+  self.split(%r{[.!?]+}).count
   end
 end
